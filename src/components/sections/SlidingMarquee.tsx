036621dce265aa6571fde56fc6f7cc1165df07c5
Alan Khalili <khalilialan@gmail.com>
2026-01-28 01:18:32 -0500

    fix: update background color in SlidingMarquee component from black to brown for improved visual consistency

--- src/components/sections/SlidingMarquee.tsx
+++ src/components/sections/SlidingMarquee.tsx
@@ -15,7 +15,7 @@ export default function SlidingMarquee() {
     <div className="my-0 flex text-custom-black text-2xl bg-linear-to-b from-transparent to-custom-ivory via-custom-ivory overflow-hidden">
       <div className="h-32 flex items-center justify-center w-full relative">
         {/* Masked Border Layer */}
-        <div className="absolute inset-0 w-full h-full mask-marquee bg-custom-black pointer-events-none opacity-20 dark:opacity-40"></div>
+        <div className="absolute inset-0 w-full h-full mask-marquee bg-custom-brown pointer-events-none opacity-20 dark:opacity-40"></div>
 
         <Marquee pauseOnHover={true} delay={1}>
           {repos
